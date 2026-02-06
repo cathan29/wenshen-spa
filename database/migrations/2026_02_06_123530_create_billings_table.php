e6c565bafd5d66a0cc7247ff8feb074e313b9fd5
Sergs Rafael Oriel <oriel.sergs@gmail.com>
2026-02-06 20:41:48 +0800

    Finished core database schema for services, queues, and billing

---
+++ database/migrations/2026_02_06_123530_create_billings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('queue_id')->constrained('queues')->onDelete('cascade');
+            $table->decimal('amount_due', 8, 2);
+            $table->decimal('discount', 8, 2)->default(0);
+            $table->decimal('total_amount', 8, 2);
+            $table->enum('payment_method', ['cash', 'gcash'])->default('cash');
+            $table->enum('payment_method', ['unpaid', 'paid'])->default('unpaid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('billings');
+    }
+};
